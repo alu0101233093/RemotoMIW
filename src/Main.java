a56bb63c7218e9dafb8b474714c2c375681d7772
Leonardo Alfonso Cruz Rodríguez <alu0101233093@ull.edu.es>
2023-09-16 09:31:52 +0200

    added mul

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,10 @@ public class Main {
             sum += i;
             System.out.println("sum = " + sum);
         }
+        int mul = 1;
+        for (int i = 1; i <= 5; i++) {
+            mul *= i;
+            System.out.println("mul = " + mul);
+        }
     }
 }
